c46561cb821494161b353f0650eb1646ecfac90c
Paul Cowie <paul.cowie@ntlworld.com>
2023-03-28 00:01:44 +0100

    http?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   integrations: [image()],
-  site: 'https://anniversary.paulcowie.co.uk',
+  site: 'http://anniversary.paulcowie.co.uk',
 });
